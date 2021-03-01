04ad291d168571850e59705b28d6c632b041a050
Moez Zouari <moez_zouari@hotmail.fr>
2021-03-01 13:05:51 +0100

    vue prive : espace membre de lutilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // si l'utilisateur connecté et il va a la page connexion , la page de son compte doit éétre affichier car il est deja conneecté
+        if ($this->getUser()) {
+            return $this->redirectToRoute('account');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
