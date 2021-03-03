dd11f71cdb199504665864a4086e502c4371d26e
Moez <moez_zouari@hotmail.fr>
2021-03-03 16:19:47 +0100

    creation de la vue panier avant lentree en tunnel dachat

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,18 +3,44 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/mon-panier', name: 'cart')]
     public function index(Cart $cart): Response
     {
-
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        /* 
+        A chaque produit je voudrais enrichir c e carte complete de data de nos produit
+        que doctrine va nous chercher dans la bd
+        */
+        $cartComplete =[];
+        // Pour dire que Id c'est ma clé et quantity est ma valeur
+        foreach ($cart->get() as $id => $quantity){
+            /* A  chaque fois qu'il entre dans foreach, il veut qu'il injecte dans cartecomplete 
+            une nouvelle entrer */
+            $cartComplete[] = [
+                /* Get le produit par id a traver doctriine qui va utiliser notre entité manager 
+                et qui va aller chercher en utilisant les requete predefeni grace a symfony */
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity'=> $quantity
+            ];
+        }
+     
+        return $this->render('cart/index.html.twig',[
+            'cart' => $cartComplete
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
