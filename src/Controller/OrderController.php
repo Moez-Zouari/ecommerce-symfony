66d9cc598301840ff337eed28d82eb46a4247145
Moez <moez_zouari@hotmail.fr>
2021-03-04 18:17:51 +0100

    tunnel dachat : sommaire de la commande et ajout du style

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'order')]
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('account_address_add');
@@ -19,9 +21,17 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
+        
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ){
+           dd($form->getData());
+        }
 
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
+
         ]);
     }
 }
