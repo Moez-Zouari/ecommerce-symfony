a4584c691b76f36b0c368135093ef20f238e09dc
Moez <moez_zouari@hotmail.fr>
2021-03-08 21:58:22 +0100

    creation d’une classe Mail() et en voie de notre premier mail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -11,8 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(SessionInterface $session): Response
+    public function index(): Response
     {
+
+        $mail = new Mail();
+        $mail->send('moez.zouari.94@gmail.com','Moez Zouari','Mon premier mail','Test 2 juste njarab w bara');
+
         return $this->render('home/index.html.twig');
     }
 }
