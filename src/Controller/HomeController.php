fc822b4ac382ceee1c63f811afefec9719fd2889
Moez <moez_zouari@hotmail.fr>
2021-03-09 16:42:50 +0100

    mettre des produits a la une pour les afficher sur homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Classe\Mail;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -11,10 +13,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-
-        return $this->render('home/index.html.twig');
+        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+        return $this->render('home/index.html.twig',[
+            'products' =>$products
+        ]);
     }
 }
